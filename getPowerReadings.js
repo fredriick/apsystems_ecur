4db8300d8e54153f5b2f5b305bdfd37b28b9c972
Fredrick <95657057+fredriick@users.noreply.github.com>
2023-06-08 15:16:17 +0100

    Update getPowerReadings.js

--- getPowerReadings.js
+++ getPowerReadings.js
@@ -1,7 +1,25 @@
-const apsystemsEcur = require("apsystems_ecur");
+// const apsystemsEcur = require("apsystems_ecur");
+
+// async function getPowerReadings() {
+//   const devices = await apsystemsEcur.getDevices();
+//   const powerReadings = [];
+
+//   for (const device of devices) {
+//     const powerReading = await device.getPowerReading();
+//     powerReadings.push(powerReading);
+//   }
+
+//   return powerReadings;
+// }
+
+// module.exports = getPowerReadings;
+
+
+const ECUR = require("apsystems_ecur");
 
 async function getPowerReadings() {
-  const devices = await apsystemsEcur.getDevices();
+  const ecur = new ECUR("192.168.1.10", 8080);
+  const devices = await ecur.getDevices();
   const powerReadings = [];
 
   for (const device of devices) {
@@ -12,4 +30,10 @@ async function getPowerReadings() {
   return powerReadings;
 }
 
-module.exports = getPowerReadings;
+(async () => {
+  const powerReadings = await getPowerReadings();
+
+  for (const powerReading of powerReadings) {
+    console.log("Current power reading for inverter", powerReading.id, "is", powerReading.power);
+  }
+})();
